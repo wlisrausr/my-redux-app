88d634bbcebbfe320636c3ebfd39c2da9e8ca82b
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-12-09 19:21:40 +0700

    Tidy up & add margin for jumbotron

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { voteAngular, voteReact, voteVuejs } from './actions';
-import './App.css';
+import React, { Component } from "react";
+import { voteAngular, voteReact, voteVuejs } from "./actions";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   constructor(props) {
@@ -10,33 +11,48 @@ class App extends Component {
 
   handleVoteAngular = () => {
     this.store.dispatch(voteAngular());
-  }
+  };
 
   handleVoteReact = () => {
     this.store.dispatch(voteReact());
-  }
+  };
 
   handleVoteVuejs = () => {
     this.store.dispatch(voteVuejs());
-  }
+  };
 
   render() {
     return (
       <div>
-        <div className="jumbotron" style={{'textAlign': 'center'}}>
-          <img src="ctsw_logo.png" height="96" alt="CodingTheSmartWay.com"></img>
+        <div className="jumbotron" style={{ textAlign: "center", marginTop: "2%" }}>
+          <img src="ctsw_logo.png" height="96" alt="CodingTheSmartWay.com" />
           <h2>What is your favorite front-end development framework 2017?</h2>
           <h4>Click on the logos below to vote!</h4>
           <br />
           <div className="row">
             <div className="col-xs-offset-3 col-xs-2">
-              <img src="angular_logo.png" height="96" alt="Angular" onClick={this.handleVoteAngular}></img>
+              <img
+                src="angular_logo.png"
+                height="96"
+                alt="Angular"
+                onClick={this.handleVoteAngular}
+              />
             </div>
             <div className="col-xs-2">
-              <img src="react_logo.png" height="96" alt="React" onClick={this.handleVoteReact}></img>
+              <img
+                src="react_logo.png"
+                height="96"
+                alt="React"
+                onClick={this.handleVoteReact}
+              />
             </div>
             <div className="col-xs-2">
-              <img src="vuejs_logo.png" height="96" alt="Vue.js" onClick={this.handleVoteVuejs}></img>
+              <img
+                src="vuejs_logo.png"
+                height="96"
+                alt="Vue.js"
+                onClick={this.handleVoteVuejs}
+              />
             </div>
           </div>
         </div>
